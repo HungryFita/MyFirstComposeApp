d5389fcedbcdaa88dd3a4edb875d540fe9360dc8
HungryFita <HungryFita@gmail.com>
2022-10-26 21:14:00 -0400

    Save the work at the end of Step 6

--- app/src/main/java/com/example/myfirstcomposeapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstcomposeapp/MainActivity.kt
@@ -3,13 +3,15 @@ package com.example.myfirstcomposeapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.myfirstcomposeapp.ui.theme.MyFirstComposeAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,27 +19,50 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MyFirstComposeAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                MyApp(modifier = Modifier.fillMaxSize())
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+private fun MyApp(
+    modifier: Modifier = Modifier,
+    names: List<String> = listOf("World", "Compose")
+) {
+    Column(
+        modifier = modifier.padding(vertical = 4.dp)
+    ) {
+        for (name in names) {
+            Greeting(name = name)
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Composable
+fun Greeting(name: String) {
+    Surface(
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp),
+        color = MaterialTheme.colorScheme.primary
+    ) {
+        Row(modifier = Modifier.padding(24.dp)) {
+            Column (modifier = Modifier.weight(1f)) {
+                Text(text = "Hello,")
+                Text(text = name)
+            }
+            ElevatedButton(
+                onClick = { /*TODO*/ }
+            ) {
+                Text(text = "Show more")
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320)
 @Composable
 fun DefaultPreview() {
     MyFirstComposeAppTheme {
-        Greeting("Android")
+        MyApp()
     }
 }
